99e7d1d1567a9638a9dc8b5564870b8a8ae5586a
wktk <mail@wktk.jp>
2013-07-28 14:06:38 +0900

    Update at *:25

--- main.rb
+++ main.rb
@@ -2,6 +2,6 @@
 require './bot'
 
 Bot.new.register do |bot|
-  bot.timer.at_each(/:[25]5/) { bot.update }
+  bot.timer.at_each(/:25/) { bot.update }
 #  bot.timer.at_each(//) { bot.reply }
 end.join
